41f87bc649fe7df677b928e4175ca3ee23c2765b
Kazi Mridul <kazimridulwebdev@gmail.com>
2026-02-21 00:59:43 +0600

    category update code

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,18 +3,20 @@ const { uploadToCloudinary } = require("../utils/cloudinaryService")
 
 const createCategory = async(req,res)=>{
     try {
-        const{name,description} = req.body
+        const{name,slug,description} = req.body
         const thumbnail = req.file
         if(!name) return res.status(400).send({message : 'Category name is required'})
+        if(!slug) return res.status(400).send({message : 'Category slug is required'})
         if(!thumbnail) return res.status(400).send({message : 'Thumbnail is requred'})
 
-        const existCategory = await categorySchema.findOne({name})
-        if(existCategory) return res.status(400).send({message : 'Category name is already exist'})
+        const existCateSlug = await categorySchema.findOne({slug})
+        if(existCateSlug) return res.status(400).send({message : 'Category name is already exist'})
 
         const imgRes =await uploadToCloudinary(thumbnail,'categories')
 
         const category = categorySchema({
             name,
+            slug,
             description,
             thumbnail :imgRes.secure_url
         })
